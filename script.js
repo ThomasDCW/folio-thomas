730341f33a383fad04098f1767f6de9051762219
Thomas <thomas.dicola@hotmail.fr>
2023-06-13 22:58:27 +0200

    refactor

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
-const smMenuBtn = document.querySelector('.main-header__sm-scr-nav-btn');
-const smMenu = document.querySelector('.main-header__sm-menu');
-const smMenuCloseBtn = document.querySelector('.main-header__sm-menu-close');
+const smMenuBtn = document.querySelector(".main-header__sm-scr-nav-btn");
+const smMenu = document.querySelector(".main-header__sm-menu");
+const smMenuCloseBtn = document.querySelector(".main-header__sm-menu-close");
 
-const smMenuLinks = document.querySelectorAll('.main-header__sm-menu-link');
-const smMenuLink1 = document.querySelector('.main-header__sm-menu-link--1');
-const smMenuLink2 = document.querySelector('.main-header__sm-menu-link--2');
-const smMenuLink3 = document.querySelector('.main-header__sm-menu-link--3');
+const smMenuLinks = document.querySelectorAll(".main-header__sm-menu-link");
+const smMenuLink1 = document.querySelector(".main-header__sm-menu-link--1");
+const smMenuLink2 = document.querySelector(".main-header__sm-menu-link--2");
+const smMenuLink3 = document.querySelector(".main-header__sm-menu-link--3");
 
-smMenuBtn.addEventListener('click', () => {
-  smMenu.style.transitionDelay = '0s';
-  smMenu.classList.add('main-header__sm-menu--active');
+smMenuBtn.addEventListener("click", () => {
+  smMenu.style.transitionDelay = "0s";
+  smMenu.classList.add("main-header__sm-menu--active");
 
-  smMenuLink1.style.transitionDelay = '.5s';
-  smMenuLink1.style.transform = 'translateY(0)';
-  smMenuLink1.style.opacity = '1';
+  smMenuLink1.style.transitionDelay = ".5s";
+  smMenuLink1.style.transform = "translateY(0)";
+  smMenuLink1.style.opacity = "1";
 
-  smMenuLink2.style.transitionDelay = '.8s';
-  smMenuLink2.style.transform = 'translateY(0)';
-  smMenuLink2.style.opacity = '1';
+  smMenuLink2.style.transitionDelay = ".8s";
+  smMenuLink2.style.transform = "translateY(0)";
+  smMenuLink2.style.opacity = "1";
 
-  smMenuLink3.style.transitionDelay = '1.1s';
-  smMenuLink3.style.transform = 'translateY(0)';
-  smMenuLink3.style.opacity = '1';
+  smMenuLink3.style.transitionDelay = "1.1s";
+  smMenuLink3.style.transform = "translateY(0)";
+  smMenuLink3.style.opacity = "1";
 });
 
 smMenuLinks.forEach((ele) => {
-  ele.addEventListener('click', () => {
-    smMenuLink3.style.transitionDelay = '.3s';
-    smMenuLink3.style.transform = 'translateY(50px)';
-    smMenuLink3.style.opacity = '0';
+  ele.addEventListener("click", () => {
+    smMenuLink3.style.transitionDelay = ".3s";
+    smMenuLink3.style.transform = "translateY(50px)";
+    smMenuLink3.style.opacity = "0";
 
-    smMenuLink2.style.transitionDelay = '.6s';
-    smMenuLink2.style.transform = 'translateY(50px)';
-    smMenuLink2.style.opacity = '0';
+    smMenuLink2.style.transitionDelay = ".6s";
+    smMenuLink2.style.transform = "translateY(50px)";
+    smMenuLink2.style.opacity = "0";
 
-    smMenuLink1.style.transitionDelay = '.9s';
-    smMenuLink1.style.transform = 'translateY(50px)';
-    smMenuLink1.style.opacity = '0';
+    smMenuLink1.style.transitionDelay = ".9s";
+    smMenuLink1.style.transform = "translateY(50px)";
+    smMenuLink1.style.opacity = "0";
 
-    smMenu.style.transitionDelay = '1.2s';
-    smMenu.classList.remove('main-header__sm-menu--active');
+    smMenu.style.transitionDelay = "1.2s";
+    smMenu.classList.remove("main-header__sm-menu--active");
 
     setTimeout(() => {
       document.getElementById(ele.name).scrollIntoView();
-    }, 1300);
+    }, 1500);
   });
 });
 
-smMenuCloseBtn.addEventListener('click', () => {
-  smMenuLink3.style.transitionDelay = '.3s';
-  smMenuLink3.style.transform = 'translateY(50px)';
-  smMenuLink3.style.opacity = '0';
+smMenuCloseBtn.addEventListener("click", () => {
+  smMenuLink3.style.transitionDelay = ".3s";
+  smMenuLink3.style.transform = "translateY(50px)";
+  smMenuLink3.style.opacity = "0";
 
-  smMenuLink2.style.transitionDelay = '.6s';
-  smMenuLink2.style.transform = 'translateY(50px)';
-  smMenuLink2.style.opacity = '0';
+  smMenuLink2.style.transitionDelay = ".6s";
+  smMenuLink2.style.transform = "translateY(50px)";
+  smMenuLink2.style.opacity = "0";
 
-  smMenuLink1.style.transitionDelay = '.9s';
-  smMenuLink1.style.transform = 'translateY(50px)';
-  smMenuLink1.style.opacity = '0';
+  smMenuLink1.style.transitionDelay = ".9s";
+  smMenuLink1.style.transform = "translateY(50px)";
+  smMenuLink1.style.opacity = "0";
 
-  smMenu.style.transitionDelay = '1.2s';
-  smMenu.classList.remove('main-header__sm-menu--active');
+  smMenu.style.transitionDelay = "1.2s";
+  smMenu.classList.remove("main-header__sm-menu--active");
 });
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  const textElement = document.getElementById('text-animation');
-  const text = 'WEB DEVELOPER';
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("text-animation");
+  const text = "WEB DEVELOPER";
   let index = 0;
 
   function showNextLetter() {
@@ -76,8 +74,25 @@ document.addEventListener('DOMContentLoaded', function() {
       textElement.textContent += text.charAt(index);
       index++;
       setTimeout(showNextLetter, 150); // Temps de délai entre chaque lettre
-    } 
+    }
   }
 
   showNextLetter();
+});
+smMenuLink1.addEventListener("click", (event) => {
+  event.preventDefault(); // Empêche le comportement par défaut du lien
+
+  // Supprimer la classe "active" de tous les liens
+  smMenuLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+
+  // Ajouter la classe "active" uniquement au lien cliqué
+  smMenuLink1.classList.add("active");
+
+  // Effectuer le défilement de la page jusqu'en haut en douceur
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
